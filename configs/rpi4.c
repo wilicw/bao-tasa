9dc525caa8de2aacd52cdfa4ce83a64e1fc37f78
William Chang <wilicw1346@gmail.com>
2023-07-21 10:14:16 +0800

    feat: init all submodules

---
+++ configs/rpi4.c
@@ -0,0 +1,142 @@
+#include <config.h>
+
+VM_IMAGE(linux_image, XSTR(BAO_DEMOS_WRKDIR_IMGS/linux.bin));
+VM_IMAGE(freertos_image, XSTR(BAO_DEMOS_WRKDIR_IMGS/freertos.bin));
+
+struct config config = {
+    
+    CONFIG_HEADER
+
+    .shmemlist_size = 1,
+    .shmemlist = (struct shmem[]) {
+        [0] = { .size = 0x00010000, }
+    },
+    
+    .vmlist_size = 2,
+    .vmlist = {
+        { 
+            .image = {
+                .base_addr = 0x20000000,
+                .load_addr = VM_IMAGE_OFFSET(linux_image),
+                .size = VM_IMAGE_SIZE(linux_image)
+            },
+
+            .entry = 0x20000000,
+
+            .platform = {
+                .cpu_num = 3,
+                
+                .region_num = 1,
+                .regions =  (struct vm_mem_region[]) {
+                    {
+                        .base = 0x20000000,
+                        .size = 0x40000000,
+                        .place_phys = true,
+                        .phys = 0x20000000
+                    }
+                },
+
+               .ipc_num = 1,
+                .ipcs = (struct ipc[]) {
+                    {
+                        .base = 0xf0000000,
+                        .size = 0x00010000,
+                        .shmem_id = 0,
+                        .interrupt_num = 1,
+                        .interrupts = (irqid_t[]) {52}
+                    }
+                },
+
+                .dev_num = 2,
+                .devs =  (struct vm_dev_region[]) {
+                    {
+                        /* GENET */
+                        .pa = 0xfd580000,
+                        .va = 0xfd580000,
+                        .size = 0x10000,
+                        .interrupt_num = 2,
+                        .interrupts = (irqid_t[]) {189, 190}  
+                    },
+                    {   
+                        /* Arch timer interrupt */
+                        .interrupt_num = 1,
+                        .interrupts = 
+                            (irqid_t[]) {27}                         
+                    }
+                },
+
+                .arch = {
+                    .gic = {
+                        .gicd_addr = 0xff841000,
+                        .gicc_addr = 0xff842000,
+                    }
+                }
+            },
+        },
+        { 
+            .image = {
+                .base_addr = 0x0,
+                .load_addr = VM_IMAGE_OFFSET(freertos_image),
+                .size = VM_IMAGE_SIZE(freertos_image)
+            },
+
+            .entry = 0x0,
+
+            .platform = {
+                .cpu_num = 1,
+                
+                .region_num = 1,
+                .regions =  (struct vm_mem_region[]) {
+                    {
+                        .base = 0x0,
+                        .size = 0x8000000 
+                    }
+                },
+
+               .ipc_num = 1,
+                .ipcs = (struct ipc[]) {
+                    {
+                        .base = 0x70000000,
+                        .size = 0x00010000,
+                        .shmem_id = 0,
+                        .interrupt_num = 1,
+                        .interrupts = (irqid_t[]) {52}
+                    }
+                },
+
+                .dev_num = 3,
+                .devs =  (struct vm_dev_region[]) {
+                    {   
+                        /* UART1 */
+                        .pa = 0xfe215000,
+                        .va = 0xff000000,
+                        .size = 0x1000,
+                        .interrupt_num = 1,
+                        .interrupts = (irqid_t[]) {125}                        
+                    },
+                    {   
+                        /* Arch timer interrupt */
+                        .interrupt_num = 1,
+                        .interrupts = 
+                            (irqid_t[]) {27}                         
+                    },
+                    {
+                        /* GPIO */
+                        .pa = 0xfe200000,
+                        .va = 0xff010000,
+                        .size = 0xf0,
+                        .interrupt_num = 1,
+                        .interrupts = (irqid_t[]) {148}
+                    }
+               },
+                .arch = {
+                    .gic = {
+                        .gicd_addr = 0xF9010000,
+                        .gicc_addr = 0xF9020000,
+                    }
+                }
+            },
+        },
+    },
+};
+
